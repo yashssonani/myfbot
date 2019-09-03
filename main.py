5af46ab9665d70604e48ebb4e016b12b29dd012d
yashssonani <49947849+yashssonani@users.noreply.github.com>
2019-09-03 23:46:55 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.dog/woof.json').json()    
+    url = contents['url']
+    return url
+
+def bop(bot, update):
+    url = get_url()
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('675873200:AAFiKbZzn9eX3V1tYTG1zhJ6KuFgM3cWgDo')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('bop',bop))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
